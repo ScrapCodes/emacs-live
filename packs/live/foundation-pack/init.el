04935e6af794c85ebf2a84cd0895598891bf1fcc
Prashant Sharma <prashant.iiith@gmail.com>
2012-06-30 14:26:42 +0530

    Removing support for popwin. Simply hate it!

--- packs/live/foundation-pack/init.el
+++ packs/live/foundation-pack/init.el
@@ -15,10 +15,8 @@
 (live-load-config-file "window-number-conf.el")
 (live-load-config-file "recentf-conf.el")
 (live-load-config-file "elisp-slime-nav-conf.el")
-;(live-load-config-file "popwin-conf.el")
 (live-load-config-file "shell-conf.el")
 (live-load-config-file "spelling-conf.el")
-;(live-load-config-file "lisp-conf.el")
+(live-load-config-file "lisp-conf.el")
 
-(when (eq system-type 'darwin)
-  (live-load-config-file "osx.el"))
+(live-load-config-file "osx.el")
